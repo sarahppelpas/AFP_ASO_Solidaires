39000ebe8c3caf385a94cc6717676d42ef221fc5
Kevin <kevinserda.pro@gmail.com>
2023-02-03 14:31:11 +0100

    Initial Commit

---
+++ AFP_ASO_Solidaire/AFP_ASO_SolidaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFP_ASO_SolidaireApp.swift
+//  AFP_ASO_Solidaire
+//
+//  Created by apprenant16 on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AFP_ASO_SolidaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
